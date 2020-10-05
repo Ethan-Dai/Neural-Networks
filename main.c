ff1709d9a4fcb6887a4c27713a39f6c70e3b8ea4
Ethan <EthanBen@msn.com>
2020-10-05 08:39:12 +0800

    prototype

--- main.c
+++ main.c
@@ -36,29 +36,31 @@ int main(void)
 	mtrace();
 	printf("memory debug enabled.\n");
 #endif
-	struct net mynet;
+	struct net net;
 	struct data_pack train_data, test_data;
-	net_setup(&mynet, 3, neu_num);
+	net_setup(&net, 3, neu_num);
 	
-	net_init(&mynet, sigmoid);
+	//net_init(&net, sigmoid);
+	net_load(&net, "net89");
 	
 	read_data(&train_data, train_images_path, train_labels_path);
 	read_data(&test_data, test_images_path, test_labels_path);
 	
-	//net_load(&mynet, filename);
 	
-	net_train(&mynet, &train_data, score, 20, 3.0, 1);
+	net_train(&net, &train_data, score, 10, 1.0, 1);
 	
-	float correct_rate = net_pack_test(&mynet, &test_data);
+	float correct_rate = net_pack_test(&net, &test_data);
 	printf("Correct rate : %.2f %% \n", correct_rate * 100);
 
-	//net_work(&mynet, &train_data, 0, true);
+	if(correct_rate > 0.90)
+		net_save(&net, "net");
+	//net_work(&net, &train_data, 0, true);
 	
 	//mnist_print(&train_data, 0);
 	
-	//net_activ_save(&mynet, "activ");
+	//net_activ_save(&net, "activ");
 
-	net_del(&mynet);
+	net_del(&net);
 	mnist_del(&train_data);
 	mnist_del(&test_data);
 
